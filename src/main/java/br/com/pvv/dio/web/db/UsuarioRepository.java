68f552e22afa1fdacdb7bdd3376d06567f4a96d7
pliniopvv <gangss2@Hotmail.com>
2022-09-09 15:04:44 -0300

    CRUD JPA OK

--- src/main/java/br/com/pvv/dio/web/db/UsuarioRepository.java
+++ src/main/java/br/com/pvv/dio/web/db/UsuarioRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import br.com.pvv.dio.web.db.model.Usuario;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "usuario")
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
